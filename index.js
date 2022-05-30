357256b7528af0a8c86f31bad0c171e3bfddbcb8
Haala <haalahadithy@gmail.com>
2022-05-30 10:02:35 -0700

    commit tree 1fe272cb8d414fcfa9a69e0478d0296b9dc10081

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mn = document.getElementById('main');
+mn.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Haala is the champion";
+document.body.append(newHeader);
